2098051be63f3e1582b4ce05ed087ca0a97cfd9e
offends <offends4@163.com>
2024-04-16 13:48:15 +0800

    Update Zabbix Script

--- install/zabbix-install.sh
+++ install/zabbix-install.sh
@@ -128,16 +128,16 @@ zabbix_uninstall5(){
     docker stop opsany-zabbix-server-st2
     docker rm opsany-zabbix-web
     docker rm opsany-zabbix-server-st2
-    rm -rf ${INSTALL_PATH}{zabbix-volume
+    rm -rf ${INSTALL_PATH}/zabbix-volume
 }
 
 zabbix_uninstall6(){
     shell_log "=====Uninstall Zabbix 6.0======"
-    docker stop opsany-zabbix-web
-    docker stop opsany-zabbix-server-st2
+    docker stop opsany-zabbix-web-6.0
+    docker stop opsany-zabbix-server-6.0
     docker stop opsany-zabbix-mysql8
-    docker rm opsany-zabbix-web
-    docker rm opsany-zabbix-server-st2
+    docker rm opsany-zabbix-web-6.0
+    docker rm opsany-zabbix-server-6.0
     docker rm opsany-zabbix-mysql8
     rm -rf ${INSTALL_PATH}/{zabbix-volume,logs/mysql8,conf/mysql8,zabbix-mysql8-volume}
 }
@@ -154,10 +154,10 @@ main(){
         zabbix_6_0_install
         ;;
     uninstall5)
-        zabbix_uninstall
+        zabbix_uninstall5
         ;;
     uninstall6)
-        zabbix_uninstall
+        zabbix_uninstall6
         ;;
         help|*)
                 echo $"Usage: $0 {5.0|6.0|uninstall5|uninstall6|help}"
