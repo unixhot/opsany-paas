29914fbc41261e4908f0b363a651ca574fe269e2
Jason Zhao <zhaoshundong@gmail.com>
2024-12-02 14:28:21 +0800

    release: v2.2.3

--- install/paas-change-domain.sh
+++ install/paas-change-domain.sh
@@ -41,6 +41,34 @@ else
     source ./install.env && rm -f install.env
 fi
 
+# Create Self-signed Server Certificate
+ssl_make(){
+    shell_log "======Init: Create Self-signed Server Certificate======"
+    # create dir for ssl
+    if [ ! -d ./conf/nginx-conf.d/ssl ];then
+      mkdir -p ./conf/nginx-conf.d/ssl
+    fi
+    cp ./conf/openssl.cnf ./conf/nginx-conf.d/ssl/
+    cd ./conf/nginx-conf.d/ssl
+    openssl genrsa -des3 -passout pass:opsany -out $NEW_DOMAIN_NAME.key 2048 >/dev/null 2>&1
+
+    #Create server certificate signing request
+    SUBJECT="/C=CN/ST=BeiJing/L=BeiJing/O=BeiJing/OU=OpsAny/CN=OpsAny"
+    openssl req -new -passin pass:opsany -subj $SUBJECT -key $NEW_DOMAIN_NAME.key -out $NEW_DOMAIN_NAME.csr >/dev/null 2>&1
+
+    #Remove password
+    mv $NEW_DOMAIN_NAME.key $NEW_DOMAIN_NAME.origin.key
+    openssl rsa -passin pass:opsany -in $NEW_DOMAIN_NAME.origin.key  -out $NEW_DOMAIN_NAME.key >/dev/null 2>&1
+
+    #Sign SSL certificate
+    openssl x509 -req -days 3650 -extfile openssl.cnf -extensions 'v3_req'  -in $NEW_DOMAIN_NAME.csr -signkey $NEW_DOMAIN_NAME.key -out $NEW_DOMAIN_NAME.crt >/dev/null 2>&1
+    openssl x509 -in ${NEW_DOMAIN_NAME}.crt -out ${NEW_DOMAIN_NAME}.pem -outform PEM >/dev/null 2>&1
+    mv ${NEW_DOMAIN_NAME}.pem ${NEW_DOMAIN_NAME}.origin.pem
+    cat ${NEW_DOMAIN_NAME}.key ${NEW_DOMAIN_NAME}.origin.pem > ${NEW_DOMAIN_NAME}.pem
+    rm -f ./conf/openssl.cnf
+}
+
+
 replace_domain(){
     shell_log "Replace PaaS config files"
     # 替换默认安装文件中的配置
@@ -63,10 +91,6 @@ replace_domain(){
     shell_log "替换nginx域名，替换后如无法访问，请自行检查nginx配置"
     sed -i "s/${OLD_DOMAIN_NAME}/${NEW_DOMAIN_NAME}/g" ${INSTALL_PATH}/conf/nginx-conf.d/opsany_paas.conf
     sed -i "s/${OLD_DOMAIN_NAME}/${NEW_DOMAIN_NAME}/g" ${INSTALL_PATH}/conf/nginx-conf.d/opsany_proxy.conf
-    cd ${INSTALL_PATH}/conf/nginx-conf.d/ssl/
-    /bin/cp ${OLD_DOMAIN_NAME}.key ${NEW_DOMAIN_NAME}.key
-    /bin/cp ${OLD_DOMAIN_NAME}.pem ${NEW_DOMAIN_NAME}.pem
-
 
     shell_log "Replace SaaS config files"
     # 替换已经安装的saas服务域名（可采用重新部署）
@@ -83,6 +107,7 @@ replace_domain(){
     sed -i "s/${OLD_DOMAIN_NAME}/${NEW_DOMAIN_NAME}/g"  ${INSTALL_PATH}/conf/opsany-saas/repo/repo-init.py
     sed -i "s/${OLD_DOMAIN_NAME}/${NEW_DOMAIN_NAME}/g"  ${INSTALL_PATH}/conf/opsany-saas/pipeline/pipeline-init.py
     sed -i "s/${OLD_DOMAIN_NAME}/${NEW_DOMAIN_NAME}/g"  ${INSTALL_PATH}/conf/opsany-saas/deploy/deploy-init.py
+    sed -i "s/${OLD_DOMAIN_NAME}/${NEW_DOMAIN_NAME}/g"  ${INSTALL_PATH}/conf/opsany-saas/code/code-init.py
 }
 
 restart_paas(){
@@ -92,7 +117,7 @@ restart_paas(){
     done
 }
 restart_saas(){
-    service_list='opsany-saas-ce-rbac opsany-saas-ce-workbench opsany-saas-ce-cmdb opsany-saas-ce-control opsany-saas-ce-job opsany-saas-ce-monitor opsany-saas-ce-devops opsany-saas-ce-cmp opsany-saas-ce-bastion opsany-saas-ce-dashboard'
+    service_list='opsany-saas-ce-rbac opsany-saas-ce-workbench opsany-saas-ce-cmdb opsany-saas-ce-control opsany-saas-ce-job opsany-saas-ce-monitor opsany-saas-ce-devops opsany-saas-ce-cmp opsany-saas-ce-bastion opsany-saas-ce-pipeline opsany-saas-ce-deploy opsany-saas-ce-repo'
     for service in $service_list;do
         shell_log "Restart $service" && docker restart $service;
     done
@@ -109,6 +134,7 @@ main(){
         shell_warning_log "./paas-change-domain.sh old_domanin_name new_domain_name"
         exit 1
     fi
+    ssl_make
     replace_domain;
     restart_paas;
     restart_saas;
