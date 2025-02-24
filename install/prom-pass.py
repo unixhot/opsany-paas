ea905341cd02a8562883a8a0dc05f2db812b94aa
Jason Zhao <zhaoshundong@gmail.com>
2025-02-24 16:31:36 +0800

    fix: delete old file

--- install/prom-pass.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-import bcrypt
-
-password = sys.argv[1]
-hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
-print(hashed_password.decode())
